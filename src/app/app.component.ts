69e9a96230b54e306e760c6a958470b15bbb8d8f
imarthaid <master.amarta@gmail.com>
2021-05-22 11:37:00 +0700

    part4 data dan property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'belajar-angular12';
+  title = 'Belajar Angular 12';
+  namaDepan = 'Andar';
+  namaBelakang = 'Pratama'
+  umur = 25
+  angka = [1, 5, 7, 3, 8]
+
+  imgUrl = 'https://images.unsplash.com/photo-1510915228340-29c85a43dcfe'
+  imgUrl2 = 'https://images.unsplash.com/photo-1610563166150-b34df4f3bcd6'
+
+  isValid = false;
+
+  biodata = {
+    namaDepan: 'Andar',
+    namaBelakang: 'Pratama',
+    umur: 25
+  }
 }
